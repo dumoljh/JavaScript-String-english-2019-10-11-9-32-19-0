82c2fab1739984c3b607895d258f192f75a9cc64
JHON AEDRIAN DUMOL (DEV-DIGITAL-ISD-OOCLL/MNL) <dumoljh@oocl.com>
2022-08-18 19:52:02 +0800

    Answer js-String

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+// Complete the following procedure, the 'name' variable in all letters to uppercase,
+// output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+console.log("' '" + name.toUpperCase() + "' '")
 
 
-// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+// Complete the following procedure, capitalizing all the words in the variable sentence 
+//and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+console.log(capitalizingFirstWords(sentence))
 
+function capitalizingFirstWords(words) {
+   var separateWord = words.toLowerCase().split(' ');
+   for (var i = 0; i < separateWord.length; i++) {
+      separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+      separateWord[i].substring(1);
+   }
+   return separateWord.join(' ');
+}
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+console.log(money.replace( /^\D+/g, ''))
